47aa370543fea7fbb3c77ff6631c4e5adaff4cbc
Caiti Lange <clange441@gmail.com>
2026-01-07 18:25:57 -0600

    muses api completed

---
+++ Back-End/api/muses.js
@@ -0,0 +1,74 @@
+//import express
+import express from "express";
+//create express router
+const museRouter = express.Router();
+//middlewares
+import requireBody from "#middlewares/requireBody";
+import requireUser from "#middlewares/requireUser";
+//utils
+import { createToken } from "#utils/jwt";
+
+//import db functions from /db/queries/muses
+import {
+  createMuse,
+  deleteMuse,
+  getAllMuses,
+  getMuseById,
+  updateMuse,
+} from "#db/queries/muses";
+
+museRouter.use(requireUser);
+
+// muses routes
+museRouter
+  .route("/")
+  .get(async (req, res) => {
+    const muse = await getAllMuses({ user_id: req.user.user_id });
+
+    res.send({ muse });
+  })
+  .post(requireBody(["name", "origin", "media_origin", "image_url"]), async (req, res) => {
+    const muse = await createMuse({
+      ...req.body,
+      user_id: req.user.user_id,
+    });
+
+    res.send({ muse });
+  });
+
+museRouter.param("id", async (req, res, next, id) => {
+  const muse = await getMuseById(id);
+
+  if (!muse) {
+    return res.status(404).send("Muse not found.");
+  }
+  req.muse = muse;
+  next();
+});
+
+//  /muses/:id routes
+museRouter
+  .route("/:id")
+  .get(async (req, res) => {
+    res.send(req.muse);
+  })
+  .put(requireBody([]), async (req, res) => {
+    const { name, origin, media_origin, img_url } = req.body;
+
+    const muse = await updateMuse({
+      muse_id: req.params.id,
+      name,
+      origin,
+      media_origin,
+      img_url,
+    });
+
+    res.send({ muse });
+  })
+  .delete(async (req, res) => {
+    const muse = await deleteMuse({ muse_id: req.params.id });
+
+    res.send({ muse });
+  });
+
+export default museRouter;
